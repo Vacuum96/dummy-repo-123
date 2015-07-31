ffdec9c48a53acdbeec1ce902f3322e574a83287
Chris Bargren <chrisb@axosoft.com>
2015-07-31 10:12:19 -0700

    moar commits plox
    kjh
    
    Conflicts:
    	test.txt
    
    conflict tests
    
    binary conflict base
    
    binary conflict left

--- stupid.cpp
+++ stupid.cpp
@@ -3,7 +3,11 @@ using namespace std;
 
 int main() {
 <<<<<<< HEAD
+<<<<<<< HEAD
 	cout << "I am very stupid" << endl;
+=======
+	cout << "I am not stupid" << endl;
+>>>>>>> d5b6309... moar commits plox
 =======
 	cout << "I am not stupid" << endl;
 >>>>>>> d5b6309... moar commits plox
